e64a700058e8415cea52c7483eb6cc4b2b9e9c19
tantrungnguyen2001 <trungnt@zigvy.com>
2026-01-01 21:29:41 +0700

    feat: implement user wishlist page with search functionality and pagination; integrate BookCard component for displaying items

--- app/user/wishlist/page.tsx
+++ app/user/wishlist/page.tsx
@@ -1,3 +1,35 @@
+import UserWrapper from "@/components/pages/user/user-wrapper";
+import BookCard from "@/components/shared/book-card";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import Pagination from "@/components/ui/pagination";
+import { Search } from "lucide-react";
+
 export default function UserWishListPage() {
-  return <div>UserWishListPage</div>;
+  return (
+    <UserWrapper header="Wishlist">
+      <InputGroup className="w-1/2">
+        <InputGroupInput placeholder="Search voucher" />
+        <InputGroupAddon>
+          <Search />
+        </InputGroupAddon>
+      </InputGroup>
+
+      <ul className="grid grid-cols-3 gap-4 mb-8">
+        {[...Array(9)].map((_, id) => (
+          <div
+            key={id}
+            className="*:h-110 *:outline-2 *:outline-transparent *:hover:outline-foreground *:transition-colors *:duration-300"
+          >
+            <BookCard />
+          </div>
+        ))}
+      </ul>
+
+      <Pagination />
+    </UserWrapper>
+  );
 }
